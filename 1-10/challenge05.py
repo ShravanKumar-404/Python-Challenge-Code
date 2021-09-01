a175c1c2bf978d58f5fb5db0d7ea0ef63962539c
Shravan <shra1.mywork@gmail.com>
2021-09-01 20:22:51 +0530

    new challenge updated

---
+++ 1-10/challenge05.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program that prints the string s without the characters located at even indices.
+
+If the string is empty or only has one character, print it intact.
+
+"""
+
+
+
+word = ""
+
+if len(word) > 1:
+  print(word[1::2])
+else:
+  print(word)
+  
+  
+  
