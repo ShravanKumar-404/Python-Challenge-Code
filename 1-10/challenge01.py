95ee945c56e47fd982a8d620855f454c9017a039
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-01 11:03:05 +0530

    changes

---
+++ 1-10/challenge01.py
@@ -0,0 +1,3 @@
+length_input = "Amazing"
+length_output = len(length_input)
+print(length_output)
---
+++ 1-10/challenge02.py
@@ -0,0 +1,9 @@
+word = "Helloooo"
+index_number = 0
+
+if (len(word) == 0):
+  print("empty string")
+elif (index_number < len(word)):
+  print(word[index_number])
+else:
+  print("index is out of range")
