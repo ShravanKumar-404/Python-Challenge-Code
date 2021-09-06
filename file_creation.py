584ae93059da394f909fbdf7401a9dcc12ad6e32
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-06 22:50:57 +0530

    changes

--- file_creation.py
+++ file_creation.py
@@ -4,4 +4,17 @@ import os
 cwd = os.getcwd()
 dir_name = input("Enter Folder name : ")
 path = os.path.join(cwd, dir_name)
-# os.mkdir(path)
+os.mkdir(path)
+os.chdir(path)
+versionStart = int(input("Enter the version to start : "))
+versionEnd = int(input("Enter the version to end : "))
+
+
+def file_creation(version):
+    filename = f"challenge{version}.py"
+    file = open(filename, "a")
+
+
+for i in range(versionStart, versionEnd + 1):
+    file_creation(i)
+    print(f"file created version : {i} ")
