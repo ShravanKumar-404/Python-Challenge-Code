8085a47076adbee8b0b78442b94263702f98a492
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-06 07:24:29 +0530

    changes

--- 21-30/challenge25.py
+++ 21-30/challenge25.py
@@ -7,7 +7,34 @@ If the lists have the same elements, print an empty list.
 If listA is an empty list, print an empty list.
 
 """
+
+#  method 1
+
+
+# list_a = [1, 2, 3, 4, 5]
+# list_b = [1, 2]
+
+# combined_list = []
+
+# for x in list_a:
+#     if x not in list_b:
+#         combined_list.append(x)
+
+# print(combined_list)
+
+# method 2
+
+# list_a = [1, 2, 3, 4, 5]
+# list_b = [1, 2]
+
+# combined_list = [x for x in list_a if x not in list_b]
+# print(combined_list)
+
+# method 3
+
+
 list_a = [1, 2, 3, 4, 5]
 list_b = [1, 2]
 
-differnce_list = []
+combined_list = list(set(list_a) - set(list_b))
+print(combined_list)
