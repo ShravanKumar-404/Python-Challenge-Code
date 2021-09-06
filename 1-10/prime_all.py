aaaf25921a536c5fe190a0009f0cfe9ee5d999ea
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-06 11:29:33 +0530

    changes

--- 1-10/prime_all.py
+++ 1-10/prime_all.py
@@ -5,14 +5,12 @@ num2 = int(input("Enter first number : "))
 num3 = int(input("Enter last number number : "))
 
 for i in range(num2, num3+1):
-  if i >= 1:
-    if (i == 1) or (i == 2):
-      print(f"{i} is prime")
-    elif(i % 2 == 0):
-      print(f"{i} not prime")
+    if i >= 1:
+        if (i == 1) or (i == 2):
+            print(f"{i} is prime")
+        elif(i % 2 == 0):
+            print(f"{i} not prime")
+        else:
+            print(f"{i} is prime")
     else:
-        print(f"{i} is prime")
-  else:
-    print(f"{i} is not greater than 1")
-  
-  
+        print(f"{i} is not greater than 1")
