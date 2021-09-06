20c7c85405774eecd93d6368ce11128ca58430c7
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-06 08:54:14 +0530

    challenges updated

--- 21-30/challenge29.py
+++ 21-30/challenge29.py
@@ -1,21 +1,19 @@
 """
 
-Write a Python program that prints the second largest value in a list.
+Write a Python program that prints the second smallest value in a list.
 
 If the list is empty or only has one element, print None.
 
 
 """
 
-# my method
 
-List = [1, 2, 3, 4, 5, 2, 3]
+List = [0, 1, 2, 3, 4, 5, 2, 3]
 
 
 if len(List) > 1:
     listSet = set(List)
     setList = list(listSet)
-    setList.sort(reverse=True)
     print(setList[1])
 else:
     print(None)
