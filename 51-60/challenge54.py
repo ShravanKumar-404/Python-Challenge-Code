9ca1f8abdedb6b7dece7c8ef9bbbda39f006bda2
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-11 18:36:30 +0530

    updated 55 challenges

--- 51-60/challenge54.py
+++ 51-60/challenge54.py
@@ -1,1 +1,26 @@
 # Welcome to the python challenges of version 54.
+
+"""
+Write a Python program that prints if a given year was (or will) be a leap year.
+
+Tip: A leap year is "a year, occurring once every four years,
+that has 366 days including February 29 as an intercalary day." (Definition by Oxford Languages).
+
+This is how you can determine if a year is a leap year or not:
+
+if (year is not divisible by 4) then (it is a common year).
+
+else if (year is not divisible by 100) then (it is a leap year)
+
+else if (year is not divisible by 400) then (it is a common year)
+
+else (it is a leap year)
+
+"""
+
+year = 1912
+
+if year % 4 != 0 and year % 400 != 0:
+    print(f"{year} is a Common Year")
+else:
+    print(f"{year} is a Leap Year")
--- 51-60/challenge55.py
+++ 51-60/challenge55.py
@@ -1,1 +1,92 @@
 # Welcome to the python challenges of version 55.
+
+# Create a Calculator
+
+
+import operator
+
+
+def operation():
+    print("Great! Now enter the operation. \nThese are the available options:")
+    print("1 - Addition \n2 - Subtraction \n3 - Multiplication \n4 - Division \n5 - Integer Division \n6 - Modulo")
+    operation1 = input("Please enter the operation to perform: ")
+
+    while not operation1.isdigit() or int(operation1) > 6:
+        print("Please enter valid operation to perform: ")
+        operation2 = input("Please enter the operation to perform: ")
+        if operation2.isdigit() and int(operation2) <= 6:
+            return int(operation2)
+
+    return int(operation1)
+
+
+def first_number_check():
+    number = input("Enter the First Number : ")
+    while not number.isdigit():
+        print("Please enter valid number")
+        number1 = input("Enter the First Number : ")
+        if number1.isdigit():
+            return int(number1)
+    return int(number)
+
+
+def second_number_check():
+    number = input("Enter the Second Number : ")
+    while not number.isdigit():
+        print("Please enter valid number")
+        number1 = input("Enter the Second Number : ")
+        if number1.isdigit():
+            return int(number1)
+    return int(number)
+
+
+def exit():
+    rcalc = input(
+        "To repeat calculation , type 'y' to continue or 'n' to exit : ")
+    if rcalc == 'y' and rcalc.isalpha():
+        calculator()
+    elif rcalc.isnumeric():
+        print("Sorry You entered number..\nThanks for the calculation.. \nBYE BYE")
+    else:
+        print("Thanks for the calculation............... \nBYE BYE")
+
+
+def calculator():
+    fnum = first_number_check()
+    snum = second_number_check()
+    opera = operation()
+
+    if opera == 1:
+        print(f"addition of {fnum}+{snum} : {fnum + snum}")
+        print(".\n.\n.\n")
+        exit()
+    elif opera == 2:
+        print(f"subtraction of {fnum}+{snum} : {fnum - snum}")
+        exit()
+    elif opera == 3:
+        print(f"multiplication of {fnum}*{snum} : {fnum * snum}")
+        exit()
+    elif opera == 4:
+        if snum == 0:
+            print("Cannot divide by Zero")
+            exit()
+        else:
+            print(f"division of {fnum}/{snum} : {fnum / snum}")
+            exit()
+    elif opera == 5:
+        if snum == 0:
+            print("Cannot divide by Zero")
+            exit()
+        else:
+            print(f"integer division of {fnum}//{snum} : {fnum // snum}")
+            exit()
+    elif opera == 6:
+        print(f"modulo of {fnum}%{snum} : {fnum % snum}")
+        exit()
+
+
+def repeat_calc():
+    calculator()
+
+
+calculator()
