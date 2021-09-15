0127ff0c21ac33f7bb258085e56ac018cccf152d
Shravan <shra1.mywork@gmail.com>
2021-09-15 20:38:26 +0530

    changes updated

--- 61-70/challenge64.py
+++ 61-70/challenge64.py
@@ -1,1 +1,27 @@
 # Welcome to the python challenges of version 64.
+
+"""
+Write a Python program that checks if a number is prime or not.
+
+If the number is prime, it should print "Prime".
+
+If the number is not prime, it should print "Not prime".
+
+"""
+"""
+finding a the given number is prime number or not.
+
+"""
+
+num1 = int(input("Enter number : "))
+
+# condition to check whether the number is greater than 1
+# number must be divisible by itself and 1
+
+if num1 >= 1:
+    if (num1 == 1) or (num1 == 2) or num1 % 2 != 0:
+        print("is prime")
+    else:
+        print("not prime")
+else:
+    print("number must be greater than 1")
--- 61-70/challenge65.py
+++ 61-70/challenge65.py
@@ -1,1 +1,24 @@
 # Welcome to the python challenges of version 65.
+
+"""
+Write a Python program that prints a triangular pattern 
+like the ones shown below in the examples.
+
+Each row must have its corresponding number of asterisks.
+The first row contains one asterisk. The second row contains 
+two asterisks. The third row contains three asterisks and so on.
+
+The number of rows should be determined by the value of n,a value 
+entered by the user.
+"""
+n = int(input("Enter the number of rows : "))
+letter = "s"
+space = " "
+# i = 1
+# while i <= n:
+#     print(letter*i)
+#     i += 1
+
+for x in range(0, n)[1::2]:
+    y = int((n-(x+1))/2)
+    print(f"{space*y}{letter*x}{space*y}")
