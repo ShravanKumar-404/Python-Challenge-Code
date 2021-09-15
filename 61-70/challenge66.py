cbf0fd48f3c4d0198b8122ea13b9c6fc9034d6e5
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-15 23:41:41 +0530

    changes updated

--- 61-70/challenge66.py
+++ 61-70/challenge66.py
@@ -8,4 +8,20 @@ If the number only has one digit, print this digit.
 """
 
 num = input("Enter the number : ")
-number = str(num)
+
+number = [i for i in num[::-1]]
+reverse = num[::-1]
+
+# awesome method for reverse algorithm it works for numbers
+
+num1 = int(num)
+reverse_num = 0
+
+while num1 > 0:
+    remainder = num1 % 10
+    reverse_num = (reverse_num*10) + remainder
+    num1 = num1 // 10
+
+print("".join(number))
+print(reverse)
+print(reverse_num)
