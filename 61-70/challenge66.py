c720d32f13e1637620125ee79adf3a5b98cf2745
Shravan <shra1.mywork@gmail.com>
2021-09-15 20:48:39 +0530

    update

--- 61-70/challenge66.py
+++ 61-70/challenge66.py
@@ -1,1 +1,11 @@
 # Welcome to the python challenges of version 66.
+
+"""
+Write a Python program that prints the digits of a number in reverse order on the same line.
+
+If the number only has one digit, print this digit.
+
+"""
+
+num = input("Enter the number : ")
+number = str(num)
