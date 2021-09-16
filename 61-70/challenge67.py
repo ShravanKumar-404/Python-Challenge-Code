ced3988e00d7a67369f8a9989be6720dea3f1791
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-16 11:31:26 +0530

    updated changes

--- 61-70/challenge67.py
+++ 61-70/challenge67.py
@@ -1,1 +1,16 @@
 # Welcome to the python challenges of version 67.
+"""
+Write a Python program that prints a string reversed using a loop.
+
+All the characters must be on the same line in reverse order.
+"""
+
+word = input("Enter the word : ")
+
+
+letters = word[::-1]
+
+for x in range(len(word)-1, -1, -1):
+    print(word[x], end="")
+
+print(letters)
