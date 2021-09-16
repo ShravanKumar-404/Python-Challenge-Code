5e3dbafb5d4df5b9d7a67f6de9d6cd4d401ab497
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-16 22:51:52 +0530

    updated changes

--- 61-70/challenge68.py
+++ 61-70/challenge68.py
@@ -1,1 +1,17 @@
 # Welcome to the python challenges of version 68.
+
+"""
+Write a Python program that prints a pyramid pattern made with asterisks.
+
+The number of rows should be determined by the value of the variable n. This value will be entered by the user.
+
+You may assume that the value of n is a positive integer.
+
+"""
+
+n = int(input("Enter the number of rows : "))
+letter = "s"
+space = " "
+
+for x in range(1, n):
+    print(f"{space*(n-x)}{x*letter}")
--- 61-70/challenge69.py
+++ 61-70/challenge69.py
@@ -1,1 +1,9 @@
 # Welcome to the python challenges of version 69.
+
+"""
+Write a Python program that prints Floyd's Triangle with n number of rows.
+
+The value of n should be entered by the user. You may assume that it is a positive integer.
+
+Floyd's Triangle is made with consecutive numbers that fill the rows of the triangle (as shown below).
+"""
