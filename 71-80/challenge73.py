5fdcb8f1a52627e712ea3bb2afcb5d72490b6467
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-18 13:50:53 +0530

    updated challenges

--- 71-80/challenge73.py
+++ 71-80/challenge73.py
@@ -1,1 +1,21 @@
 # Welcome to the python challenges of version 73.
+
+"""
+Write a Python program that calculates and prints the nth Fibonacci number.
+
+The value of n represents the position of the element in the sequence.
+
+The initial value of n should be 0.
+
+You may assume that the value of n is a non-negative integer.
+"""
+n = 10
+lists = []
+
+for x in range(0, n):
+    if x == 0 or x == 1:
+        lists.append(x)
+    else:
+        lists.append((lists[-1] + lists[-2]))
+
+print(lists)
