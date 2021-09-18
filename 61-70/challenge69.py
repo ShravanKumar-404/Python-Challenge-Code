dd26216d5ed0933d050bf825887aeb1f207d1d15
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-18 11:14:43 +0530

    update

--- 61-70/challenge69.py
+++ 61-70/challenge69.py
@@ -9,18 +9,24 @@ Floyd's Triangle is made with consecutive numbers that fill the rows of the tria
 """
 rows = int(input("Enter th number of rows : "))
 letters = "*"
-number = 2
+number = 1
 lists = [i*letters for i in range(1, rows+1)]
 list3 = []
 
-for x in lists:
-    if len(x) == 1:
-        print(1)
-    elif len(x) > 1:
-        list2 = []
-        for i in x:
-            list2.append(number)
-            number += 1
-        y = (str(list2))[1:-1]
-        z = y.replace(",", "")
-        print(z)
+# for x in lists:
+#     if len(x) == 1:
+#         print(1)
+#     elif len(x) > 1:
+#         list2 = []
+#         for i in x:
+#             list2.append(number)
+#             number += 1
+#         y = (str(list2))[1:-1]
+#         z = y.replace(",", "")
+#         print(z)
+
+for i in range(1, rows+1):
+    for x in range(0, i):
+        print(number, end=" ")
+        number += 1
+    print()
