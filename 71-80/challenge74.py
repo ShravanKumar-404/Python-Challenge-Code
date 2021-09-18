dfc5181fcd3caff0a7070b5f5179a6b26b5a25a8
Shravan Kumar Tela <shra1.mywork@gmail.com>
2021-09-18 20:51:49 +0530

    updated

--- 71-80/challenge74.py
+++ 71-80/challenge74.py
@@ -1,1 +1,21 @@
 # Welcome to the python challenges of version 74.
+
+"""
+Write a Python program that calculates and prints the value of the factorial of the number num using recursion.
+
+The factorial of a number x is denoted by x! and it is equal to x * (x-1) * (x-2) * ... * 1,
+the product of all positive integers less than or equal to the number.
+
+The value of 0! is equal to 1 by mathematical convention.
+"""
+num = int(input("Enter the number : "))
+
+
+def fac(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fac(n-1)
+
+
+print(fac(num))
--- 71-80/challenge75.py
+++ 71-80/challenge75.py
@@ -1,1 +1,19 @@
 # Welcome to the python challenges of version 75.
+
+"""
+Write a Python program that calculates and prints the sum of the digits of a positive integer num.
+
+The program must find the sum recursively.
+
+If the integer has only one digit, print the integer as the total sum.
+
+"""
+
+num = input("Enter the number : ")
+
+
+def sums(n):
+    if len(n) == 1:
+        return n
+    else:
+        return
